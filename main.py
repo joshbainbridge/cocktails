936709ccae4862b6e9b45bf2ed919a9861a3da7a
Josh Bainbridge <josh.bainbridge@gmail.com>
2022-10-23 17:56:15 +0100

    Added back standard inference from text prompt, commented out img2img implementation

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
+from torch import Generator
 from PIL import Image
-from diffusers import StableDiffusionImg2ImgPipeline
+from diffusers import StableDiffusionPipeline, StableDiffusionImg2ImgPipeline
 
-url = "input.png"
-prompt = "Floating heads surrounding a cocktail drink"
+#init_path = "input.png"
+#init_image = Image.open(init_path).convert("RGB")
+#init_image = init_image.resize((768, 512))
+
+prompt = '''
+The consolation of rain. Figures. Oil painting.
+'''
 
 device = "mps"
 model_id_or_path = "./stable-diffusion-v1-4"
 
-pipe = StableDiffusionImg2ImgPipeline.from_pretrained(model_id_or_path)
+#pipe = StableDiffusionImg2ImgPipeline.from_pretrained(model_id_or_path)
+pipe = StableDiffusionPipeline.from_pretrained(model_id_or_path)
 pipe = pipe.to(device)
 
 def dummy(images, **kwargs):
@@ -15,8 +22,12 @@ def dummy(images, **kwargs):
 
 pipe.safety_checker = dummy
 
-init_image = Image.open(url).convert("RGB")
-init_image = init_image.resize((768, 512))
+generator = Generator().manual_seed(2147483647)
 
-images = pipe(prompt=prompt, init_image=init_image, strength=0.75, guidance_scale=7.5).images
+#images = pipe(prompt=prompt, init_image=init_image, strength=0.75,
+#              guidance_scale=7.5).images
+
+images = pipe(prompt, guidance_scale=7.5, height=512, width=768,
+              num_images_per_prompt=1, generator=generator).images
+
 images[0].save("output.png")
